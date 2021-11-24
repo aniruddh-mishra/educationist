96eb59764843c3cb93984a5dfeb8658ed79d9366
Aniruddh Mishra <aniruddh@MacBook-Pro.local>
2021-11-23 19:17:18 -0600

    content page includes search functionality

--- public/js/content.js
+++ public/js/content.js
@@ -67,92 +67,32 @@ function createCard(id, link, title, subject, author, tags) {
     holder.appendChild(card)
 }
 
-// function refresh(ban) {
-//     if (counter >= 2) {
-//         if (limit) {
-//             alert(
-//                 'You have passed the refresh limit! All further refreshes will simply be filters'
-//             )
-//             limit = false
-//         }
-//         filter()
-//         return
-//     }
-
-//     if (!ban) {
-//         counter += 1
-//         setTimeout(() => {
-//             counter = 0
-//             limit = true
-//         }, 15 * 60 * 1000)
-//     }
-
-//     dbRef
-//         .once('value', (data) => {
-//             if (data.exists()) {
-//                 contents = data.val()
-//                 filter(ban)
-//             } else {
-//                 console.log('No data available')
-//             }
-//         })
-//         .catch((error) => {
-//             console.log(error)
-//             alert(
-//                 'Due to an unforseen error, we were unable to get you the data we needed to show you our content. If you are banned from Educationist Servers due to spam, please wait 15 days to try again!'
-//             )
-//         })
-// }
-
-// function filter(search) {
-//     const tag = document.getElementById('search').value
-//     const subjectSearch = document.getElementById('subjects').value
-//     var cards = []
-//     var subjects = []
-//     for (subject in contents) {
-//         subjects.push(subject)
-//         if (
-//             subjectSearch !== 'none' &&
-//             subject.toLowerCase() !== subjectSearch.toLowerCase()
-//         ) {
-//             continue
-//         }
-//         var tags = [subject.toLowerCase()]
-//         subject = contents[subject]
-//         for (type in subject) {
-//             tags.push(type.toLowerCase())
-//             type = subject[type]
-//             for (age in type) {
-//                 tags.push(age)
-//                 if (tag !== '' && !tags.includes(tag.toLowerCase())) {
-//                     tags.splice(2, 1)
-//                     continue
-//                 }
-//                 age = type[age]
-//                 for (content in age) {
-//                     content = age[content]
-//                     const link = content[0]
-//                     const title = content[1]
-//                     cards.push(createCard(link, title, tags))
-//                 }
-//                 tags.splice(2, 1)
-//             }
-//             tags.splice(1, 1)
-//         }
-//         tags.splice(0, 1)
-//     }
-//     if (document.getElementById('not-found')) {
-//         document.getElementById('not-found').remove()
-//     }
-//     if (cards.length === 0) {
-//         var error = document.createElement('div')
-//         error.id = 'not-found'
-//         error.innerHTML =
-//             'There were no matches for this search... Please try again with a different query.'
-//         document.querySelector('.main-body').appendChild(error)
-//     }
-//     insertCards(cards, subjects, search)
-// }
+async function search() {
+    const query = document.getElementById('search').value
+    if (query == '') {
+        return getContent()
+    }
+    const client = algoliasearch(
+        'M7ZXC6YNGS',
+        '92c6702e4476ae4bd6246ecf3a75d8a0'
+    )
+    const index = client.initIndex('content_catalog')
+    var results = await index.search(query)
+    holder.innerHTML = ''
+    if (results.hits == []) {
+        holder.innerHTML = 'No results for your query'
+    }
+    for (result of results.hits) {
+        createCard(
+            result.objectID,
+            result.link,
+            result.title,
+            result.subject,
+            result.author,
+            [result.type, result.age]
+        )
+    }
+}
 
 function openLink(link) {
     window.open(link, '_blank').focus()
@@ -183,5 +123,3 @@ async function upvote(id, button) {
     button.disabled = true
     alert('You have successfully upvoted this document!')
 }
-
-// // document.getElementById('banner-image').setAttribute('src', 'https://cdn.educationisttutoring.org/images/content-curation/' + (Math.floor(Math.random() * 9) + 1) + '.svg')
