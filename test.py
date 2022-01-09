e7c0445ba0f3803e01c4e44cbd539fe3cef8af83
Aniruddh Mishra <aniruddh@MacBook-Pro.local>
2022-01-09 12:22:22 -0600

    no more test python code

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost/transfer-data'
-myobj = {'eid': '8fVK', 'username': 'thescientist'}
-headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-x = requests.post(url, json=myobj, headers=headers)
-
-print(x.text)
