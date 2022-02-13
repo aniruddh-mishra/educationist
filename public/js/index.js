541cd9e6d849910c8f95ada7e6534ad73bf42c39
educationistpres <80840010+educationistpres@users.noreply.github.com>
2022-02-13 07:46:25 -0600

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,7 @@ async function placeData(data, dates, subjects, attendance) {
     if (dates) {
         createBlock(
             'Volunteer Hours',
-            [Number((dates[2] / 60).toFixed(1)) + ' Hours', 'More information can be found in the classes tab.'],
+            [Number((dates[2] / 60).toFixed(1)) + ' Hours', '<a style="text-decoration: underline" src="/classes">More information</a>'],
             'small'
         )
     }
