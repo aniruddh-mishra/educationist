f67847349127d783288b4fd89df1644ddc29129b
Aniruddh Mishra <aniruddh@macbook-pro.lan>
2022-03-13 15:11:38 -0500

    discord down

--- index.js
+++ index.js
@@ -123,12 +123,15 @@ app.get('/announcements/:issue', async (request, response) => {
 })
 
 app.get('/discord', (request, response) => {
-    const url =
-        'https://discord.com/api/oauth2/authorize?client_id=' +
-        process.env['CLIENT_ID'] +
-        '&scope=identify%20guilds.join&response_type=code&redirect_uri=' +
-        encodeURIComponent(processURL + '/discord/auth')
-    response.redirect(url)
+    // const url =
+    //     'https://discord.com/api/oauth2/authorize?client_id=' +
+    //     process.env['CLIENT_ID'] +
+    //     '&scope=identify%20guilds.join&response_type=code&redirect_uri=' +
+    //     encodeURIComponent(processURL + '/discord/auth')
+    // response.redirect(url)
+    response.send(
+        'We are currently working on getting our discord back online. Please try again later!'
+    )
 })
 
 // MAKE IT A POST REQUEST TO GET THIS STUFF, SEND CODE TO CLIENT
