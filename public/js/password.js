0c5ab71fcd9b8a4fb133003f87b2af8a3c61587e
Aniruddh Mishra <aniruddh@MacBook-Pro.local>
2022-03-17 17:54:05 -0500

    admin page bug

--- public/js/password.js
+++ public/js/password.js
@@ -40,7 +40,8 @@ function check(password, redirect) {
             return
         }
         document.querySelector('body').style.display = 'none'
-        document.querySelector('body').innerHTML = this.response
+        document.querySelector('body').innerHTML =
+            this.response + document.querySelector('body').innerHTML
         var s = document.createElement('script')
         s.setAttribute('src', '../js/root.js')
         s.onload = function () {
@@ -51,6 +52,32 @@ function check(password, redirect) {
             date.setTime(date.getTime() + 30 * 24 * 60 * 60 * 1000)
             const expires = 'expires=' + date.toUTCString()
             document.cookie = 'admin=' + password + '; ' + expires + '; path=/'
+            const mode = localStorage.getItem('lightmode')
+            if (mode === 'true') {
+                document.getElementById('lightmode-btn').innerHTML = 'Dark Mode'
+                var cssRoot = document.querySelector(':root')
+                const lightMode = {
+                    '--background': 'white',
+                    '--basic-font': 'black',
+                    '--invert': 'rgb(92, 90, 90)',
+                    '--background-standout': '#89cab6',
+                    '--background-block': 'rgb(30, 34, 32)',
+                    '--background-block-hover': 'rgb(37, 41, 39)',
+                    '--help-menu': 'var(--background-block)',
+                }
+                document
+                    .getElementById('theme')
+                    .setAttribute('content', 'white')
+                document
+                    .getElementById('logo-img')
+                    .setAttribute(
+                        'src',
+                        'https://cdn.educationisttutoring.org/images/dark-logos/educationist.png'
+                    )
+                for (key of Object.keys(lightMode)) {
+                    cssRoot.style.setProperty(key, lightMode[key])
+                }
+            }
         }
         document.head.appendChild(s)
     }
